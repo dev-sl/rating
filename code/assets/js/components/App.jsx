d842838935be005b14a1312587e354dba0afe013
dev-sl <gm.lion@bigmir.net>
2018-11-18 16:05:44 +0200

    Added fetch data from backend

--- code/assets/js/components/App.jsx
+++ code/assets/js/components/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import Preloader from "./Preloader";
+import Chronicles from "./Chronicles";
 
 export default class App extends React.Component
 {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            preloaderEnbled: true,
+            chronicles: null,
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            fetch('/api/v1/chronicles')
+                .then(response => response.json())
+                .then(
+                    data => {
+                        this.setState({
+                            preloaderEnbled: false,
+                            chronicles: data,
+                        })
+                    }
+                );
+        }, 2000);
+
+    }
+
     render() {
+        if (!this.state.preloaderEnbled && this.state.chronicles !== null) {
+            return (
+                <Chronicles chronicles={this.state.chronicles}/>
+            )
+        }
+
         return (
             <Preloader/>
         );
